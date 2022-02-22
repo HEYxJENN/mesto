9d281bcab4386e8ddc510454e6338dce5ac94c84
HEYxJENN <95577576+HEYxJENN@users.noreply.github.com>
2022-02-22 17:01:38 +0300

    Bugfix:Final?

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const popup = document.querySelector(".popup");
 const openPopupButton = document.querySelector(".profile__edit");
 const closePopupButton = document.querySelector(".popup__close");
-const SaveButton = document.querySelector(".popup__save");
+const saveButton = document.querySelector(".popup__save");
 const maybename = document.querySelector(".popup__name");
 const maybecaption = document.querySelector(".popup__about");
 const namenow = document.querySelector(".profile__name"); 
@@ -18,35 +18,25 @@ const closepopup = function () {
 } ;
 
 const closePopupArea = function(event) {
-  console.log(event.target, event.currentTarget)
   if (event.target !== event.currentTarget) {
     return
   }
   closepopup(event);
   }
   
+
 openPopupButton.addEventListener("click", openpopup);
 closePopupButton.addEventListener("click", closepopup);
 popup.addEventListener ("click",closePopupArea)
 
-savepopup = function() {
-const itsaname = maybename.value;
-const itsacaption = maybecaption.value;
-console.log (itsaname);
-console.log (itsacaption);
-namenow.textContent = itsaname;
-captionnow.textContent = itsacaption;
-console.log (namenow);
-console.log (captionnow);
-closepopup () ;
-console.log (maybename.placeholder)
-} ;
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  const itsaname = maybename.value;
+  const itsacaption = maybecaption.value;
+  namenow.textContent = itsaname;
+  captionnow.textContent = itsacaption;
+  closepopup () ;
+}
 
-SaveButton.addEventListener("click", savepopup);
-
-
-form.addEventListener('keypress', function(event) {
-  if (event.keyCode == 13) {
-    savepopup(); }
-} );
+form.addEventListener("submit", formSubmitHandler);
 

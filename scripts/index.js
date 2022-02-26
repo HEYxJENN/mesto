ce3818bd939cff458988692c408fa1424a33ce47
HEYxJENN <95577576+HEYxJENN@users.noreply.github.com>
2022-02-26 12:53:43 +0300

    fix: финал. сердца. разрешение. отступы.кнопки

--- scripts/index.js
+++ scripts/index.js
@@ -2,41 +2,39 @@ const popup = document.querySelector(".popup");
 const openPopupButton = document.querySelector(".profile__edit");
 const closePopupButton = document.querySelector(".popup__close");
 const saveButton = document.querySelector(".popup__save");
-const maybename = document.querySelector(".popup__name");
-const maybecaption = document.querySelector(".popup__about");
-const namenow = document.querySelector(".profile__name"); 
+const maybename = document.querySelector("#entername");
+const maybecaption = document.querySelector("#enterabout");
+const namenow = document.querySelector(".profile__name");
 const captionnow = document.querySelector(".profile__caption");
 const form = document.querySelector(".popup__forms");
 
 const openpopup = function () {
   maybename.placeholder = namenow.textContent;
   maybecaption.placeholder = captionnow.textContent;
-  popup.classList.add("popup_opened");  
-} ;
-const closepopup = function () {
-  popup.classList.remove("popup_opened");  
-} ;
+  popup.classList.add("popup_opened");
+};
 
-const closePopupArea = function(event) {
+const closepopup = function () {
+  popup.classList.remove("popup_opened");
+};
+
+const closePopupArea = function (event) {
   if (event.target !== event.currentTarget) {
-    return
+    return;
   }
   closepopup(event);
-  }
-  
+};
 
-openPopupButton.addEventListener("click", openpopup);
-closePopupButton.addEventListener("click", closepopup);
-popup.addEventListener ("click",closePopupArea)
-
-function formSubmitHandler (evt) {
+function formSubmitHandler(evt) {
   evt.preventDefault();
   const itsaname = maybename.value;
   const itsacaption = maybecaption.value;
   namenow.textContent = itsaname;
   captionnow.textContent = itsacaption;
-  closepopup () ;
+  closepopup();
 }
 
 form.addEventListener("submit", formSubmitHandler);
-
+openPopupButton.addEventListener("click", openpopup);
+closePopupButton.addEventListener("click", closepopup);
+popup.addEventListener("click", closePopupArea);
